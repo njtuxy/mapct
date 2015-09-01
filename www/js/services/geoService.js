04c87f54dc2f1a3144dc10051513caf9711d716a
Yan Xia <njtuxy@hotmail.com>
2015-09-01 14:49:06 -0700

    working with geofire integrated

---
+++ www/js/services/geoService.js
@@ -0,0 +1,18 @@
+/**
+ * Created by yxia on 9/1/15.
+ */
+
+angular.module('starter')
+  .service('GeoFireBaseService', function ($geofire) {
+
+    var geo = $geofire(new Firebase('https://xygeofire.firebaseio.com'));
+
+    this.insertGeoFireData = function (key, location) {
+      return geo.$set(key, location);
+    };
+
+    this.getGeoFireData = function(key){
+      return geo.$get(key);
+    };
+
+  })
